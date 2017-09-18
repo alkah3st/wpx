91f85c79a8fc3f214fc1c5b1a75dbcdde43a7b40
alkah3st <daniel@dquinn.net>
2017-09-18 12:44:23 -0400

    replaces @font-size mixin with vw-based mixin; removes /mobile/ styles in favor of @breakpoint mixin; new instructions regarding data-module['my-module'] classes for modules

--- index.php
+++ index.php
@@ -11,4 +11,8 @@ get_header(); ?>
 
 <h1>Home</h1>
 
+<h2 class="testme">Calculate Height Test</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sit amet tellus nec est pellentesque cursus. Praesent ullamcorper libero ac nibh malesuada bibendum. Nam in pellentesque felis. Integer vitae pellentesque elit. Donec efficitur pulvinar odio, eu vestibulum odio commodo nec. Nulla tempor sit amet sapien eu pharetra. Etiam eu fermentum augue, a tempor nunc. Morbi in lorem sit amet magna tincidunt congue ac at nunc. Donec vehicula diam non tortor fermentum ultricies. Proin gravida fringilla tortor, eu ultrices ipsum auctor non.</p>
+
 <?php get_footer(); ?>
