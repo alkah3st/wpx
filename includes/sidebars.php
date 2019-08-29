075f878fa23d1f67ff2347ba15b141ba973f9b0c
alkah3st <daniel@dquinn.net>
2019-08-29 14:05:09 -0400

    quarantines custom acf stuff until acf is loaded

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -24,39 +24,44 @@ function register_sidebars() {
 		'after_widget'  => '</section>',
 	);
 
-	// look for any Pages that have "Create Sidebar" checked
-	$all_pages = new \WP_Query(array(
-		'post_type'=>"page",
-		'posts_per_page'=>-1,
-		'no_found_rows'=>true,
-		'update_post_term_cache'=>false
-	));
+	if (class_exists('acf')) {
 
-	if ( $all_pages->have_posts() ) :
-		while ( $all_pages->have_posts() ) :
-			$all_pages->the_post();
-			global $post;
-			$has_sidebar = get_field("field_56648d683aa4d", $post->ID);
-			if ($has_sidebar) {
-				$sidebars[$post->post_name] = array(
-					'name'          => __( get_the_title($post->ID), 'wpx' ),
-					'id'            => $post->post_name,
-					'description'   => 'Sidebar unique to the '.get_the_title($post->ID).' page.',
-					'class'         => '',
-					'before_widget' => '<section id="%1$s" class="widget tinymce %2$s">',
-					'after_widget'  => '</section>',
-					'before_title'  => '<h2 class="widget-title">',
-					'after_title'   => '</h3>'
-				);
-			}
-		endwhile;
-	endif;
+		// look for any Pages that have "Create Sidebar" checked
+		$all_pages = new \WP_Query(array(
+			'post_type'=>"page",
+			'posts_per_page'=>-1,
+			'no_found_rows'=>true,
+			'update_post_term_cache'=>false
+		));
 
-	wp_reset_postdata();
+		if ( $all_pages->have_posts() ) :
+			while ( $all_pages->have_posts() ) :
+				$all_pages->the_post();
+				global $post;
+				$has_sidebar = get_field("field_56648d683aa4d", $post->ID);
+				if ($has_sidebar) {
+					$sidebars[$post->post_name] = array(
+						'name'          => __( get_the_title($post->ID), 'wpx' ),
+						'id'            => $post->post_name,
+						'description'   => 'Sidebar unique to the '.get_the_title($post->ID).' page.',
+						'class'         => '',
+						'before_widget' => '<section id="%1$s" class="widget tinymce %2$s">',
+						'after_widget'  => '</section>',
+						'before_title'  => '<h2 class="widget-title">',
+						'after_title'   => '</h3>'
+					);
+				}
+			endwhile;
+		endif;
+
+		wp_reset_postdata();
+
+	}
 
 	foreach ( (array) $sidebars as $sidebar ) {
 		register_sidebar( $sidebar );
 	}
 
 }
+
 add_action( 'widgets_init', '\WPX\Sidebars\register_sidebars' );
