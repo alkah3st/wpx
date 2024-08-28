07adaef48dab31b43305d14c0551182ee1b07907
Daniel Quinn <daniel@dquinn.net>
2024-08-27 21:51:09 -0400

    adding support for preview images in custom blocks

--- templates/blocks/custom-block.php
+++ templates/blocks/custom-block.php
@@ -8,17 +8,26 @@
  * @version 1.0
  */
 
+$attrs = $is_preview ? ' ' : get_block_wrapper_attributes();
 $testimonial = (get_field('testimonial') ? get_field('testimonial') : 'Testimonial text goes here...');
 $author = (get_field('author') ? get_field('author') : 'Jane Smith');
 $role = (get_field('role') ? get_field('role') : 'CEO');
 
-?>
+if( isset( $block['data']['wpx_preview_image'] )  ) : 
+	echo '<img src="'. $block['data']['wpx_preview_image'] .'" style="width:100%; height:auto;">';
+else : ?>
 
-<div <?php echo get_block_wrapper_attributes() ?>>
+<div <?php echo $attrs; ?>>
 
-	<blockquote>
-		<p><?php echo $testimonial; ?></p>
-		<cite><?php echo $author; ?>, <?php echo $role; ?></cite>
-	</blockquote>
+	<div class="<?php \WPX\Blocks\custom_block_classes(); ?>">
+
+		<blockquote>
+			<p><?php echo $testimonial; ?></p>
+			<cite><?php echo $author; ?>, <?php echo $role; ?></cite>
+		</blockquote>
+
+	</div>
 
 </div>
+
+<?php endif;
